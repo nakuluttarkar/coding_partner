b829344e00243b0eac121ca91abc6878702a9b87
nakuluttarkar <nakul.uttarkar@gmail.com>
2025-11-02 20:59:01 +0530

    changed models

--- agent/graph.py
+++ agent/graph.py
@@ -23,14 +23,14 @@ user_prompt = "Create a simple calculator web app using html, css, and javascrip
 def planner_agent(state: dict) -> dict:
     print("\n ------- ENTERING PLANNER AGENT-------\n")
     user_prompt = state["user_prompt"]
-    resp = groq_model_gpt_120b.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+    resp = groq_model_gpt_20b.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
     print(resp)
     return {"plan": resp}
 
 def architect_agent(state: dict) -> dict:
     print("\n ------- ENTERING ARCHITECT AGENT-------\n")
     plan: Plan = state["plan"]
-    resp = groq_model_gpt_120b.with_structured_output(TaskPlan, method="function_calling").invoke(architect_prompt(plan))
+    resp = groq_model_gpt_20b.with_structured_output(TaskPlan, method="function_calling").invoke(architect_prompt(plan))
     if resp is None:
         raise ValueError("No response from Architect")
     print(resp)
